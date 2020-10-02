c3044077c12060eb106ed6743a409a81a030f7fd
Estelle OLAWINSKI <estelle@fr.mpitech.com>
2020-10-02 15:21:06 +0200

    v-model // v-for

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <div class="container">
+      <div class="row">
+        <p class="jumbotron w-100">
+          Login: <input type="text" v-model="user" /> <br /><br />
+          Vous êtes connecté en tant que {{ user }}.
+        </p>
+      </div>
+
+      <div class="row">
+        <div class="col-3" v-for="(produit, index) in produits" :key="index">
+          <p class="jumbotron">
+            {{ produit }}
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      user: "Estelle",
+      produits: ["Pizza", "Cheese", "Tacos", "Kebab"],
+    };
+  },
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
